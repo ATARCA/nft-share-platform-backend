92e0f8e3fc12a6e10d00c0dd223e523b8bf87868
Martin Moravek <martin.m.moravek@gmail.com>
2022-02-14 12:05:12 +0100

    cleanup

--- app.ts
+++ app.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import demoRouter from './routes/demoRouter';
 import { unknownEndpoint, errorHandler, requestLogger } from './utils/middleware';
-import { ApolloServer, gql } from 'apollo-server';
-
-//TODO log all requests
 
 const app = express();
 app.use(express.json());
